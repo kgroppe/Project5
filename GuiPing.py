0c78cf068c3d37bccfb7da14370bd3486a26e88b
Kate Groppe <kgroppe@gmail.com>
2023-11-20 11:18:15 -0600

    adding code

--- GuiPing.py
+++ GuiPing.py
@@ -5,3 +5,23 @@ import socket
 
 from time import gmtime, strftime
 
+def pingScan(event):
+    if hostEnd.GetValue():
+        dlg = wx.MessageDialog(mainWin, "Invalid Local Host Selection",
+                               "Confirm:", wx.OK | wx.ICON_EXCLAMATION)
+        result = dlg.ShowModal()
+        dlg.Destroy()
+        return
+
+    mainWin.StatusBar.SetStatusText("Executing Ping Sweep .... Please Wait")
+
+    utcStart = gmtime()
+    utc = strftime("%a, %d %b %Y %X + 0000", utcStart)
+    results.AppendText("\n\nPing Sweep Started: "+utc+"\n\n")
+    baseIP = str(ipaRange.GetValue())+"."+str(ipbRange.GetVaule())"."+str(ipcRange.GetVaule())+"."
+
+    ipRange = []
+
+    for i in range(hostStart.GetVaule(), (hostEnd.GetValue()+1)):
+        ipRange.append(baseIP+str(i))
+    
