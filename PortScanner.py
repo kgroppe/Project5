f579c1ffbbcfab683065e9bab016d41075209389
Kate Groppe <kgroppe@gmail.com>
2023-11-24 12:30:48 -0600

    editing PortScanner

--- PortScanner.py
+++ PortScanner.py
@@ -2,12 +2,17 @@ import wx
 import sys
 import ping3
 from socket import *
+
 from time import gmtime, strftime
 
-
-def portScan(event):
-    if portEnd.GetValue() < portStart.GetValue():
-        dlg = wx.MessageDialog(mainWin, "Invalid Host Port Selection", "Confirm", wx.OK | wx.ICON_EXCLAMATION)
+def protScan(event):
+    if portEnd.GetValue() < portStart.GetVaule():
+        dlg = wx.MessageDialog(mainWin,"Invalid Host Port Selection", "Confirm", wx.OK|wx.ICON_EXCLAMATION)
         result = dlg.ShowModal()
         dlg.Destroy()
-    return
+        return
+
+    mainWin.StatusBar.SetStatus9'Executing Port Scan ... Please Wait'_)
+
+    
+
